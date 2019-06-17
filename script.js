45b8b28f97c895d4ef1c1f30c1f3625234f4dcea
Rusydy <rusydymuhiddin@gmail.com>
2019-06-17 20:47:41 +0700

    done

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function compareNumbers (firstNumber, secondNumber) {
-  // Your code here
-}
-
-// TEST CASES
-console.log(compareNumbers(5, 8)); // true
-console.log(compareNumbers(5, 3)); // false
-console.log(compareNumbers(4, 4)); // -1
-console.log(compareNumbers(3, 3)); // -1
-console.log(compareNumbers(17, 2)); // false
+  if (firstNumber >  secondNumber) {
+    return false
+  } else if (firstNumber < secondNumber) {
+    return true
+  } else return -1
+ }
+ 
+ // TEST CASES
+ console.log(compareNumbers(5, 8)); // true
+ console.log(compareNumbers(5, 3)); // false
+ console.log(compareNumbers(4, 4)); // -1
+ console.log(compareNumbers(3, 3)); // -1
+ console.log(compareNumbers(17, 2)); // false
