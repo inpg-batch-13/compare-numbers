f7b4967080e6d592b3bfc90b72d00060dd087da9
danielevanc <danielevanc2@gmail.com>
2019-06-17 20:39:39 +0700

    compare numbers

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function compareNumbers (firstNumber, secondNumber) {
-  // Your code here
+   
+  if (firstNumber < secondNumber){
+    return true
+  } else if (firstNumber > secondNumber){ 
+    return false
+  } else if (firstNumber === secondNumber){ 
+    return -1
+  }
 }
 
 // TEST CASES
